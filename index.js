1121ee764001438f67bd740bee6c058f5cdfa28b
Behruz <behruzbahtiyorov10@gmail.com>
2023-02-18 15:37:00 +0500

    new

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const timerEl = document.getElementById("timer")
-const hours = +prompt("hours", "0")
-const minutes = +prompt("minutes", "0")
-const seconds = +prompt("seconds", "0")
+
+const timeFromLocalStorage = localStorage.getItem("time")
+
+if (!timeFromLocalStorage) {
+    const hours = +prompt("hours", "0")
+    const minutes = +prompt("minutes", "0")
+    const seconds = +prompt("seconds", "0")
+    start(hours, minutes, seconds)
+} else {
+    start(...JSON.parse(timeFromLocalStorage))
+}
 
 function start() {
     const time = Array.from(arguments)
@@ -13,7 +21,7 @@ function start() {
         if (t == 0 && i == 2) {
             if (arr[1] == 0) {
                 if (arr[0] == 0) {
-                    throw Error("ended")
+                    return start(hours, minutes, seconds + 1)
                 }
                 arr[0] = arr[0] - 1
                 arr[2] = 60
@@ -25,10 +33,11 @@ function start() {
     })
 
     timerEl.innerHTML = time.join(":")
+    localStorage.setItem("time", JSON.stringify(time))
 
     setTimeout(() => {
         start(time[0], time[1], time[2] - 1)
     }, 1000)
 }
 
-start(hours, minutes, seconds)
+
